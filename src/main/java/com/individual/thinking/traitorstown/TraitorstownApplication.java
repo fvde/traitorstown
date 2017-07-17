0087eba3bf420318341f445fbd58c32f4b7b6f2e
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2017-07-17 16:47:08 +0200

    Initial Project

---
+++ src/main/java/com/individual/thinking/traitorstown/TraitorstownApplication.java
@@ -0,0 +1,12 @@
+package com.individual.thinking.traitorstown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraitorstownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraitorstownApplication.class, args);
+	}
+}

44ea24768b05423be14f883ba7feb1c97b219dd3
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2017-07-22 21:13:11 +0200

    Fix type of game not found exception

--- src/main/java/com/individual/thinking/traitorstown/game/exceptions/GameNotFoundException.java
+++ src/main/java/com/individual/thinking/traitorstown/game/exceptions/GameNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class GameNotFoundException extends Throwable {
+public class GameNotFoundException extends Exception {
     public GameNotFoundException(String message) {
         super(message);
     }

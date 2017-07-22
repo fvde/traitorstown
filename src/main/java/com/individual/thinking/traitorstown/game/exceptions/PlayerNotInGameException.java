8a367d8f9d8276d08ea14b3f8a479deb1e1113cb
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2017-07-22 22:31:00 +0200

    fix exception type

--- src/main/java/com/individual/thinking/traitorstown/game/exceptions/PlayerNotInGameException.java
+++ src/main/java/com/individual/thinking/traitorstown/game/exceptions/PlayerNotInGameException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class PlayerNotInGameException extends Throwable {
+public class PlayerNotInGameException extends Exception {
     public PlayerNotInGameException(String message) {
         super(message);
     }

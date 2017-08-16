fb8e881c759829841747620ef2d542f72742e935
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2017-08-16 22:36:18 +0200

    fix user identity generation

--- src/main/java/com/individual/thinking/traitorstown/user/User.java
+++ src/main/java/com/individual/thinking/traitorstown/user/User.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique=true)
